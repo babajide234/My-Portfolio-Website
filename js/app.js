64b8187098bf9c251be2c51ac5ef88439ab00785
Babajide Tomoshegbo <babajidet234@gmail.com>
2019-08-04 23:16:13 +0100

    Seprated the scss into modules

---
+++ js/app.js
@@ -0,0 +1,4 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'node_modules/particle.js/particle.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
